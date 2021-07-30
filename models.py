c8683b55763141a208f52ef094b584ed46c5d38d
atharvsahasrabudhe <68095892+atharvsahasrabudhe@users.noreply.github.com>
2021-07-30 18:40:02 +0530

    Update models.py

--- models.py
+++ models.py
@@ -24,3 +24,15 @@ class Product(models.Model):
     mfg_date=models.DateField()
     expiry_date=models.DateField()
     image = models.ImageField(upload_to='shop/images', default="")
+
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zip_code = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111, default="")
